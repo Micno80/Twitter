004af13095259c7fb642ee106ed97bf8125485c9
Michal <michal@student.edu>
2019-04-07 22:00:42 +0200

    Day 1 ex 1

---
+++ Documents/WAR_PHP_W_12_OOP-master/1_Zadania/Dzien_1/1_Podstawy_obiektowosci/Calculator.php
@@ -0,0 +1,52 @@
+<?php
+class Calculator
+{
+    public $history = [];
+
+    public function add(float $num1, float $num2)
+    {
+        $result = $num1 + $num2;
+        array_push($this->history, "added $num1 to $num2 got $result");
+        return $result;
+    }
+
+    public function multiply(float $num1, float $num2): float {
+        $result = $num1 * $num2;
+        array_push($this->history, "multiplied $num1 with $num2 got $result");
+        return $result;
+    }
+
+    public function subtract(float $num1, float $num2)
+    {
+        $result = $num1 - $num2;
+        array_push($this->history, "substarcted $num1 from $num2 got $result");
+        return $result;
+    }
+
+    public function divide(float $num1, float $num2): float {
+        $result = $num1 / $num2;
+        array_push($this->history, "divided $num1 by $num2 got $result");
+        return $result;
+    }
+
+    public function printOperations()
+    {
+        foreach ($this->history as $record) {
+            echo $record . "<br>";
+        }
+    }
+
+    public function clearOperations()
+    {
+        $this->history = [];
+    }
+}
+$res = new Calculator();
+$res->add(18,3);
+$res->subtract(18,3);
+$res->multiply(18,3);
+$res->divide(18,3);
+$res->printOperations();
+$res->clearOperations();
+$res->printOperations();
+
